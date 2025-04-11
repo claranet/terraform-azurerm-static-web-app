eeb0f121bebd733cb5fbdf6ff303bf2e95d422cf
Jérôme Respaut <jerome.respaut@claranet.com>
2025-04-11 17:51:12 +0200

    feat: add Custom Domains binding option

--- outputs-swa.tf
+++ outputs-swa.tf
@@ -8,3 +8,8 @@ output "default_hostname" {
   value       = azurerm_static_web_app.main.default_host_name
   description = "Azure Static Web Apps default hostname."
 }
+
+output "resource_custom_domains" {
+  value       = azurerm_static_web_app_custom_domain.main
+  description = "Azure Static Web Apps custom domains."
+}
